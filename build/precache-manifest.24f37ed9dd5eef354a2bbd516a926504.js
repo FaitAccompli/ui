675ae6ad244e06cce091ec7788ab5befea43fbd2
Ray Mart Montesclaros <raymart.montesclaros@gmail.com>
2020-08-14 21:25:05 +0800

    added customized via ui files

---
+++ build/precache-manifest.24f37ed9dd5eef354a2bbd516a926504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36380cbcf27d92a0085a5634e8b1aa20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3bc0fb7e967cd5d42ec",
+    "url": "/static/js/2.8eb49a18.chunk.js"
+  },
+  {
+    "revision": "63748a42c768e6232479f8dd8348a100",
+    "url": "/static/js/2.8eb49a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041449f2ad769aea638e",
+    "url": "/static/js/main.fe468d34.chunk.js"
+  },
+  {
+    "revision": "457afbf6f094802ddf65",
+    "url": "/static/js/runtime-main.299df88b.js"
+  }
+]);
